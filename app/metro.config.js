13dd5847bc4f8d6a345a88b2e0f201d9a0a09e3e
melissa.inmex <melissa.inmex@gmail.com>
2026-03-11 22:34:01 +0000

    firebase before deployment

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,31 +1,7 @@
-// Learn more: https://docs.expo.dev/guides/monorepos/
-const { getDefaultConfig } = require('@expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Resolve the workspace root and the app directory
-const workspaceRoot = path.resolve(__dirname, '..');
-const appDirectory = __dirname;
-
-// Add the monorepo configuration
-config.watchFolders = [workspaceRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(appDirectory, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
-
-// Add support for absolute imports
-config.resolver.alias = {
-  ...config.resolver.alias,
-  '@': path.resolve(appDirectory, 'src'),
-  '@assets': path.resolve(appDirectory, 'assets'),
-  '@components': path.resolve(appDirectory, 'src', 'components'),
-  '@screens': path.resolve(appDirectory, 'src', 'screens'),
-  '@lib': path.resolve(appDirectory, 'src', 'lib'),
-  '@hooks': path.resolve(appDirectory, 'src', 'hooks'),
-  '@utils': path.resolve(appDirectory, 'src', 'utils'),
-  '@constants': path.resolve(appDirectory, 'src', 'constants'),
-};
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
 
 module.exports = config;
